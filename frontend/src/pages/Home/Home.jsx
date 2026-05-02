ae506174ba9defaadda3ae3e1280a1bb4afe842d
manishchaudhary5761 <mchaudhary5761@gmail.com>
2026-05-02 10:38:53 +0530

    Fix import case for FoodDisplay component

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react'
 import './Home.css'
 import Header from '../../components/Header/Header.jsx'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu.jsx'
-import FoodDisplay from '../../components/foodDisplay/foodDisplay.jsx'
+import FoodDisplay from '../../components/foodDisplay/FoodDisplay.jsx'
 import AppDownload from '../../components/AppDownload/AppDownload.jsx'
 
 
